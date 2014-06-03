c6dc35a88d0b7b57724cfb930b46ec6fa2ea1d91
Canop <cano.petrole@gmail.com>
2014-06-03 08:03:06 +0200

    fixed mix of tabs and spaces

--- JSON.prune.js
+++ JSON.prune.js
@@ -13,7 +13,7 @@
 // The code is based on Douglas Crockford's code : https://github.com/douglascrockford/JSON-js/blob/master/json2.js
 // No effort was done to support old browsers. JSON.prune will fail on IE8.
 (function () {
-    'use strict';
+	'use strict';
 
 	var DEFAULT_MAX_DEPTH = 6;
 	var DEFAULT_ARRAY_MAX_LENGTH = 50;
@@ -47,59 +47,59 @@
 	Date.prototype.toPrunedJSON = Date.prototype.toJSON;
 	String.prototype.toPrunedJSON = String.prototype.toJSON;
 
-    var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
-        escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
-        meta = {    // table of character substitutions
-            '\b': '\\b',
-            '\t': '\\t',
-            '\n': '\\n',
-            '\f': '\\f',
-            '\r': '\\r',
-            '"' : '\\"',
-            '\\': '\\\\'
-        };
+	var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
+		escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
+		meta = {	// table of character substitutions
+			'\b': '\\b',
+			'\t': '\\t',
+			'\n': '\\n',
+			'\f': '\\f',
+			'\r': '\\r',
+			'"' : '\\"',
+			'\\': '\\\\'
+		};
 
-    function quote(string) {
-        escapable.lastIndex = 0;
-        return escapable.test(string) ? '"' + string.replace(escapable, function (a) {
-            var c = meta[a];
-            return typeof c === 'string'
-                ? c
-                : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-        }) + '"' : '"' + string + '"';
-    }
+	function quote(string) {
+		escapable.lastIndex = 0;
+		return escapable.test(string) ? '"' + string.replace(escapable, function (a) {
+			var c = meta[a];
+			return typeof c === 'string'
+				? c
+				: '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+		}) + '"' : '"' + string + '"';
+	}
 
-    function str(key, holder, depthDecr, arrayMaxLength) {
-        var i, k, v, length, partial, value = holder[key];
-        if (value && typeof value === 'object' && typeof value.toPrunedJSON === 'function') {
-            value = value.toPrunedJSON(key);
-        }
+	function str(key, holder, depthDecr, arrayMaxLength) {
+		var i, k, v, length, partial, value = holder[key];
+		if (value && typeof value === 'object' && typeof value.toPrunedJSON === 'function') {
+			value = value.toPrunedJSON(key);
+		}
 
-        switch (typeof value) {
-        case 'string':
-            return quote(value);
-        case 'number':
-            return isFinite(value) ? String(value) : 'null';
-        case 'boolean':
-        case 'null':
-            return String(value);
-        case 'object':
-            if (!value) {
-                return 'null';
-            }
-            if (depthDecr<=0 || seen.indexOf(value)!==-1) {
+		switch (typeof value) {
+		case 'string':
+			return quote(value);
+		case 'number':
+			return isFinite(value) ? String(value) : 'null';
+		case 'boolean':
+		case 'null':
+			return String(value);
+		case 'object':
+			if (!value) {
+				return 'null';
+			}
+			if (depthDecr<=0 || seen.indexOf(value)!==-1) {
 				return '"-pruned-"';
 			}
 			seen.push(value);
-            partial = [];
-            if (Object.prototype.toString.apply(value) === '[object Array]') {
-                length = Math.min(value.length, arrayMaxLength);
-                for (i = 0; i < length; i += 1) {
-                    partial[i] = str(i, value, depthDecr-1, arrayMaxLength) || 'null';
-                }
-                return  '[' + partial.join(',') + ']';
-            }
-            iterator(value, function(k) {
+			partial = [];
+			if (Object.prototype.toString.apply(value) === '[object Array]') {
+				length = Math.min(value.length, arrayMaxLength);
+				for (i = 0; i < length; i += 1) {
+					partial[i] = str(i, value, depthDecr-1, arrayMaxLength) || 'null';
+				}
+				return  '[' + partial.join(',') + ']';
+			}
+			iterator(value, function(k) {
 				try {
 					v = str(k, value, depthDecr-1, arrayMaxLength);
 					if (v) partial.push(quote(k) + ':' + v);
@@ -107,9 +107,9 @@
 					// this try/catch due to forbidden accessors on some objects
 				}				
 			});
-            return '{' + partial.join(',') + '}';
-        }
-    }
+			return '{' + partial.join(',') + '}';
+		}
+	}
 
 	JSON.prune = function (value, depthDecr, arrayMaxLength) {
 		if (typeof depthDecr == "object") {
@@ -123,12 +123,12 @@
 			iterator = forEachEnumerableOwnProperty;
 		}
 		seen = [];
-        depthDecr = depthDecr || DEFAULT_MAX_DEPTH;
-        arrayMaxLength = arrayMaxLength || DEFAULT_ARRAY_MAX_LENGTH;
+		depthDecr = depthDecr || DEFAULT_MAX_DEPTH;
+		arrayMaxLength = arrayMaxLength || DEFAULT_ARRAY_MAX_LENGTH;
 		return str('', {'': value}, depthDecr, arrayMaxLength);
-    };
-    
-    JSON.prune.log = function() {
+	};
+	
+	JSON.prune.log = function() {
 		console.log.apply(console,  Array.prototype.slice.call(arguments).map(function(v){return JSON.parse(JSON.prune(v))}));
 	}
 	JSON.prune.forEachProperty = forEachProperty; // you might want to also assign it to Object.forEachProperty
